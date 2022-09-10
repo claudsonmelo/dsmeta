d6a9e5cec79cf8deaa272a8635117c1ef7245532
Claudson dos Santos Melo <claudson@tst.jus.br>
2022-09-09 23:55:54 -0300

    Database ajustado

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,9 +1,10 @@
 package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,16 @@ public class SaleController {
 	private SaleService service;
 
 	@GetMapping
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(Pageable pageable) {
+		return service.findSales(pageable);
 	}
+/*
+	@Autowired
+	private SmsService smsService;
+
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		//smsService.sendSms(id);
+	}
+*/
 }
--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,14 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.awt.print.Pageable;
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.twilio.base.Page;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
+
 
 }
